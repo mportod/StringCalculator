8d6de0e202b3bb946af9704076b62362043e9246
mportod <mporto@domingoalonsogroup.com>
2024-05-01 19:37:07 +0200

    refactor: ♻️ simplified add method

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -7,20 +7,11 @@ public class StringCalculator
         if (string.IsNullOrEmpty(numbersSeparatedByComma))
             return 0;
 
-        if (!numbersSeparatedByComma.Contains(","))
-        {
-            var number = Convert.ToInt32(numbersSeparatedByComma);
-            if (number == 3)
-                return 3;
+        var numbers = numbersSeparatedByComma
+            .Split(',')
+            .Select(int.Parse)
+            .ToList();
 
-            return 4;
-        }
-        else
-        {
-            if (numbersSeparatedByComma == "2,3")
-                return 5;
-
-            return 6;
-        }
+        return numbers.Sum();
     }
 }
