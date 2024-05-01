0c4c7c5f7f5271dcbfce3c366acfbe65247c8439
mportod <mporto@domingoalonsogroup.com>
2024-05-02 01:41:48 +0200

    refactor: ✅ reduced production code

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -6,17 +6,12 @@ public class StringCalculator
 {
     public int Add(string numbersAndDelimiters)
     {
-        if (IsEmptyString(numbersAndDelimiters))
+        if (string.IsNullOrEmpty(numbersAndDelimiters))
             return 0;
 
         var numbers = GetNumbers(numbersAndDelimiters);
         ValidateNumbers(numbers);
         return numbers.Sum();
-    }
-
-    private static bool IsEmptyString(string numbersAndDelimiters)
-    {
-        return string.IsNullOrEmpty(numbersAndDelimiters);
     }
 
     private List<int> GetNumbers(string numbersAndDelimiters)
@@ -27,9 +22,10 @@ public class StringCalculator
         var numbers = onlyNumbers
             .Split(delimiters.First())
             .Select(int.Parse)
+            .Where(n => n <= 1000)
             .ToList();
 
-        return GetNumbersLessThanThousand(numbers);
+        return numbers;
     }
 
     private static string GetNumbersReplacedWithOneDelimiter(List<string> delimiters, string newDelimiter, string stringWithoutSpecification)
@@ -40,33 +36,14 @@ public class StringCalculator
         return stringWithoutSpecification;
     }
 
-    private static List<int> GetNumbersLessThanThousand(List<int> numbers)
+    private void ValidateNumbers(List<int> numbers)
     {
-        return numbers.Where(n => n <= 1000).ToList();
-    }
-
-    private static void ValidateNumbers(List<int> numbers)
-    {
-        if (ContainsNegativeNumbers(numbers))
+        if (numbers.Any(n => n < 0))
         {
-            var negativeNumbers = GetNegativeNumbers(numbers);
-            var negativeNumbersAsString = GetNumbersOnStringWithDelimiter(negativeNumbers, ",");
+            var negativeNumbers = numbers.Where(n => n < 0).ToList();
+            var negativeNumbersAsString = string.Join(",", negativeNumbers);
             throw new ArgumentException($"negatives not allowed ({negativeNumbersAsString})");
         }
-    }
-    private static List<int> GetNegativeNumbers(List<int> numbers)
-    {
-        return numbers.Where(n => n < 0).ToList();
-    }
-    
-    private static bool ContainsNegativeNumbers(List<int> numbers)
-    {
-        return numbers.Any(n => n < 0);
-    }
-    
-    private static string GetNumbersOnStringWithDelimiter(List<int> numbers, string delimiter)
-    {
-        return String.Join(delimiter, numbers.ToArray());
     }
 
     private string GetNumbersWithoutDelimitersSpecification(string numbers)
@@ -74,26 +51,18 @@ public class StringCalculator
         if (!numbers.StartsWith("//"))
             return numbers;
 
-        if (numbers.Contains("[") && numbers.Contains("]"))
-        {
-            var firstNumberIndex = numbers.LastIndexOf(']') + 2;
-            return numbers.Substring(firstNumberIndex);
-        }
-        else
-        {
-            var firstNumberIndex = numbers.LastIndexOf('\n') + 1;
-            return numbers.Substring(firstNumberIndex);
-        }
+        var firstNumberIndex = numbers.LastIndexOf('\n') + 1;
+        return numbers.Substring(firstNumberIndex);
     }
-    
-    private static List<string> GetDelimiters(string numbers)
+
+    private List<string> GetDelimiters(string numbers)
     {
         var delimiters = new List<string>() { ",", "\n" };
         if (numbers.Contains("//"))
         {
             if (numbers.Contains("[") && numbers.Contains("]"))
             {
-                Regex regex = new Regex(@"\[(.+?)\]");
+                var regex = new Regex(@"\[(.+?)\]");
                 var delimits = regex.Matches(numbers);
                 delimiters.AddRange(delimits.Select(delimit => delimit.Groups[1].Value));
             }
