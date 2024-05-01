432fb31af7f552e54be0c079319acda0735d7787
mportod <mporto@domingoalonsogroup.com>
2024-05-01 20:21:14 +0200

    refactor: ♻️ simplified add method

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -4,29 +4,47 @@ public class StringCalculator
 {
     public int Add(string numbersSeparatedByDelimiters)
     {
-        if (string.IsNullOrEmpty(numbersSeparatedByDelimiters))
+        if (IsEmptyString(numbersSeparatedByDelimiters))
             return 0;
 
-        if (numbersSeparatedByDelimiters.StartsWith("//"))
+        if (HasSpecificDelimiter(numbersSeparatedByDelimiters))
         {
-            var delimiter = numbersSeparatedByDelimiters[2];
-            var numbers = numbersSeparatedByDelimiters
-                .Substring(4)
-                .Split(delimiter)
-                .Select(int.Parse)
-                .ToList();
-            
-            return numbers.Sum();
+            return AddWithSpecificDelimiter(numbersSeparatedByDelimiters);
         }
-        else
-        {
-            var delimiters = new List<string> { "\n", "," };
-            var numbers = numbersSeparatedByDelimiters
-                .Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries)
-                .Select(int.Parse)
-                .ToList();
 
-            return numbers.Sum();
-        }
+        return AddWithDefaultDelimiters(numbersSeparatedByDelimiters);
+    }
+
+    private static bool IsEmptyString(string numbersSeparatedByDelimiters)
+    {
+        return string.IsNullOrEmpty(numbersSeparatedByDelimiters);
+    }
+
+    private static int AddWithDefaultDelimiters(string numbersSeparatedByDelimiters)
+    {
+        var delimiters = new List<string> { "\n", "," };
+        var numbers = numbersSeparatedByDelimiters
+            .Split(delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+
+        return numbers.Sum();
+    }
+
+    private static int AddWithSpecificDelimiter(string numbersSeparatedByDelimiters)
+    {
+        var delimiter = numbersSeparatedByDelimiters[2];
+        var numbers = numbersSeparatedByDelimiters
+            .Substring(4)
+            .Split(delimiter)
+            .Select(int.Parse)
+            .ToList();
+            
+        return numbers.Sum();
+    }
+
+    private static bool HasSpecificDelimiter(string numbersSeparatedByDelimiters)
+    {
+        return numbersSeparatedByDelimiters.StartsWith("//");
     }
 }
